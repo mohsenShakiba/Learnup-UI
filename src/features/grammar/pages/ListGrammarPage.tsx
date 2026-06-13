d92ceb6dc7fd1a4e6a8b90b54a9cc74ba8dcfefb
Ehsan Morshedzadeh <ehsan.morshedzadeh@gmail.com>
2026-06-13 18:04:44 +0330

    Add GrammarCard enhancements and new article image

--- src/features/grammar/pages/ListGrammarPage.tsx
+++ src/features/grammar/pages/ListGrammarPage.tsx
@@ -1,6 +1,8 @@
-import { Container } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 import { GrammarsService } from "../../../api/Learnup";
+import { Scaffold } from "../../../shared/components/Scaffold";
 import GrammarCard from "../components/GrammarCard";
 
 export default function ListGrammarPage() {
@@ -9,17 +11,29 @@ export default function ListGrammarPage() {
     queryFn: () => GrammarsService.getMobileGrammars1(),
   });
 
+  const orderedGrammars = useMemo(
+    () => grammarQuery.data ?? [],
+    [grammarQuery.data],
+  );
+
   if (grammarQuery.isLoading || grammarQuery.isFetching) {
     return <>...isloading</>;
   }
 
   return (
-    <>
-      <Container>
-        {grammarQuery.data?.map((i) => (
+    <Scaffold
+      header={
+        <Stack>
+          <Typography color="primary">گرامرها</Typography>
+        </Stack>
+      }
+      maxWidth="sm"
+    >
+      <Stack spacing={2}>
+        {orderedGrammars.map((i) => (
           <GrammarCard grammar={i} key={i.id} />
         ))}
-      </Container>
-    </>
+      </Stack>
+    </Scaffold>
   );
 }

f913462f017a0cecc437d61837c6061e05b911b2
m.shakiba <work.shakiba@gmail.com>
2026-06-30 19:44:46 +0330

    fix: adjust borderRadius in BottomNav and reduce padding in Scaffold

--- src/shared/components/Scaffold.tsx
+++ src/shared/components/Scaffold.tsx
@@ -42,7 +42,7 @@ export function Scaffold ({
           py: disablePadding ? 0 : 2,
           px: disablePadding ? 0 : 2,
           boxSizing: "border-box",
-          pb: 10,
+          pb: 9,
 
         }}
       >
